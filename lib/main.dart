8ade1cc236e0593015cdac7126ae0df78ae87a18
pplomith <plomitallopaolo@gmail.com>
2022-11-23 10:03:28 +0100

    update files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mindcare/widget_tree.dart';
+import 'package:mindcare/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MindCare());
 }
 
