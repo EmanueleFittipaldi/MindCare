b74ecfe20484e71ac98f2e254497a9a25a533586
pplomith <plomitallopaolo@gmail.com>
2022-11-25 17:29:38 +0100

    update image_upload

--- lib/image_upload.dart
+++ lib/image_upload.dart
@@ -5,20 +5,26 @@ import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class ImageUpload {
-  pickImage() async {
+  pickFile(String filetype) async {
     final _imagePicker = ImagePicker();
     final XFile? image;
 
     //verifico i permessi per accedere alla galleria
     await Permission.photos.request();
     var premissionStatus = await Permission.photos.status;
-
     if (premissionStatus.isGranted || premissionStatus.isLimited) {
-      image = await _imagePicker.pickImage(
-          source: ImageSource.gallery,
-          requestFullMetadata: false,
-          maxHeight: 150,
-          maxWidth: 150);
+      if (filetype.toLowerCase() == 'image') {
+        image = await _imagePicker.pickImage(
+            source: ImageSource.gallery,
+            requestFullMetadata: false,
+            maxHeight: 150,
+            maxWidth: 150);
+      } else if (filetype.toLowerCase() == 'video') {
+        image = await _imagePicker.pickVideo(source: ImageSource.gallery);
+      } else {
+        image = null;
+      }
+
       if (image == null) {
         return null;
       }
@@ -48,6 +54,31 @@ class ImageUpload {
       uploadTask = ref.putData(await file.readAsBytes(), metadata);
       imageUrl = await (await uploadTask).ref.getDownloadURL();
       return imageUrl;
+    } catch (e) {
+      print(e.toString());
+    }
+    return null;
+  }
+
+  uploadVideo(String video) async {
+    final _firebaseStorage = FirebaseStorage.instance;
+    String? videoUrl;
+
+    var file = File(video);
+    //caricamento in Firebase
+    final videoName = video.split('/').last;
+    final type = videoName.split('.').last;
+    final metadata = SettableMetadata(
+      contentType: 'video/$type',
+      customMetadata: {'picked-file-path': file.path},
+    );
+    UploadTask uploadTask;
+    Reference ref = FirebaseStorage.instance.ref().child('videos/$videoName');
+
+    try {
+      uploadTask = ref.putData(await file.readAsBytes(), metadata);
+      videoUrl = await (await uploadTask).ref.getDownloadURL();
+      return videoUrl;
     } catch (e) {
       print(e.toString());
     }
