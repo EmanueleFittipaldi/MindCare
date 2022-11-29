b84de8aa37c6b24ea03341889bdd071f3b61d47a
pplomith <plomitallopaolo@gmail.com>
2022-11-29 17:37:44 +0100

    update creazione paziente con email esistente e login

--- lib/gestione_paziente/aggiunta_paziente.dart
+++ lib/gestione_paziente/aggiunta_paziente.dart
@@ -30,6 +30,7 @@ class _AggiuntaPazienteWidgetState extends State<AggiuntaPazienteWidget> {
   TextEditingController? controllerCognome;
   TextEditingController? controllerUsername;
   TextEditingController? controllerPassword;
+  TextEditingController? controllerData;
 
   late bool passwordVisibility;
   final formKey = GlobalKey<FormState>();
@@ -42,6 +43,7 @@ class _AggiuntaPazienteWidgetState extends State<AggiuntaPazienteWidget> {
     controllerCognome = TextEditingController();
     controllerUsername = TextEditingController();
     controllerPassword = TextEditingController();
+    controllerData = TextEditingController();
     passwordVisibility = false;
   }
 
@@ -51,6 +53,7 @@ class _AggiuntaPazienteWidgetState extends State<AggiuntaPazienteWidget> {
     controllerCognome?.dispose();
     controllerUsername?.dispose();
     controllerPassword?.dispose();
+    controllerData?.dispose();
     super.dispose();
   }
 
@@ -205,7 +208,8 @@ class _AggiuntaPazienteWidgetState extends State<AggiuntaPazienteWidget> {
                                       0, 15, 0, 5),
                                   child: TextFormField(
                                     validator: (value) {
-                                      if (value == null || value.isEmpty) {
+                                      String val = value!.replaceAll(' ', '');
+                                      if (value.isEmpty || val.isEmpty) {
                                         return 'Inserisci un nome!';
                                       }
                                       return null;
@@ -264,7 +268,8 @@ class _AggiuntaPazienteWidgetState extends State<AggiuntaPazienteWidget> {
                                       0, 15, 0, 5),
                                   child: TextFormField(
                                     validator: (value) {
-                                      if (value == null || value.isEmpty) {
+                                      String val = value!.replaceAll(' ', '');
+                                      if (value.isEmpty || val.isEmpty) {
                                         return 'Inserisci un cognome!';
                                       }
                                       return null;
@@ -318,122 +323,128 @@ class _AggiuntaPazienteWidgetState extends State<AggiuntaPazienteWidget> {
                                         ),
                                   ),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsetsDirectional.fromSTEB(
-                                      0, 15, 0, 5),
-                                  child: Container(
-                                    width: double.infinity,
-                                    height: 52,
-                                    decoration: BoxDecoration(
-                                      color: FlutterFlowTheme.of(context)
-                                          .secondaryBackground,
-                                      borderRadius: BorderRadius.circular(15),
-                                      border: Border.all(
-                                        color: FlutterFlowTheme.of(context)
-                                            .borderColor,
+                                Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: Padding(
+                                        padding: EdgeInsetsDirectional.fromSTEB(
+                                            0, 15, 10, 0),
+                                        child: TextFormField(
+                                          validator: (value) {
+                                            if (value == null ||
+                                                value.isEmpty) {
+                                              return 'Inserisci una data di nascita!';
+                                            }
+                                            return null;
+                                          },
+                                          controller: controllerData,
+                                          autofocus: true,
+                                          readOnly: true,
+                                          obscureText: false,
+                                          decoration: InputDecoration(
+                                            labelText: 'Data di nascita',
+                                            hintStyle:
+                                                FlutterFlowTheme.of(context)
+                                                    .bodyText2,
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color:
+                                                    FlutterFlowTheme.of(context)
+                                                        .borderColor,
+                                                width: 1,
+                                              ),
+                                              borderRadius:
+                                                  BorderRadius.circular(15),
+                                            ),
+                                            focusedBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color:
+                                                    FlutterFlowTheme.of(context)
+                                                        .borderColor,
+                                                width: 1,
+                                              ),
+                                              borderRadius:
+                                                  BorderRadius.circular(15),
+                                            ),
+                                            errorBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color:
+                                                    FlutterFlowTheme.of(context)
+                                                        .borderErrorColor,
+                                                width: 1,
+                                              ),
+                                              borderRadius:
+                                                  BorderRadius.circular(15),
+                                            ),
+                                            focusedErrorBorder:
+                                                OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color:
+                                                    FlutterFlowTheme.of(context)
+                                                        .borderErrorColor,
+                                                width: 1,
+                                              ),
+                                              borderRadius:
+                                                  BorderRadius.circular(15),
+                                            ),
+                                          ),
+                                          style: FlutterFlowTheme.of(context)
+                                              .bodyText1
+                                              .override(
+                                                fontFamily: 'IBM Plex Sans',
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.normal,
+                                              ),
+                                        ),
                                       ),
                                     ),
-                                    child: Padding(
-                                      padding:
-                                          const EdgeInsetsDirectional.fromSTEB(
-                                              10, 0, 12, 0),
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            datePicked == null
+                                    InkWell(
+                                      onTap: () async {
+                                        await DatePicker.showDatePicker(context,
+                                            showTitleActions: true,
+                                            onConfirm: (date) {
+                                          setState(() {
+                                            datePicked = date;
+                                            controllerData!.text = datePicked ==
+                                                    null
                                                 ? 'Data di nascita'
                                                 : DateFormat('dd-MM-yyyy')
                                                     .format(DateTime.parse(
-                                                        datePicked.toString())),
-                                            style: FlutterFlowTheme.of(context)
-                                                .bodyText1
-                                                .override(
-                                                  fontFamily: 'IBM Plex Sans',
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.normal,
-                                                ),
-                                          ),
-                                          InkWell(
-                                            onTap: () async {
-                                              await DatePicker.showDatePicker(
-                                                  context,
-                                                  showTitleActions: true,
-                                                  onConfirm: (date) {
-                                                setState(
-                                                    () => datePicked = date);
-                                              },
-                                                  currentTime:
-                                                      getCurrentTimestamp,
-                                                  minTime: DateTime(0, 0, 0),
-                                                  maxTime: DateTime(
-                                                    DateTime.now().year,
-                                                    DateTime.now().month,
-                                                    DateTime.now().day,
-                                                  ));
-                                            },
-                                            child: const Icon(
-                                              Icons.date_range_outlined,
-                                              color: Color(0xFF57636C),
-                                              size: 24,
-                                            ),
-                                          ),
-                                        ],
+                                                        datePicked.toString()));
+                                          });
+                                        },
+                                            currentTime: getCurrentTimestamp,
+                                            minTime: DateTime(0, 0, 0),
+                                            maxTime: DateTime(
+                                              DateTime.now().year,
+                                              DateTime.now().month,
+                                              DateTime.now().day,
+                                            ));
+                                      },
+                                      child: const Icon(
+                                        Icons.date_range_outlined,
+                                        color: Color(0xFF57636C),
+                                        size: 30,
                                       ),
                                     ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsetsDirectional.fromSTEB(
-                                      0, 15, 0, 10),
-                                  child: FFButtonWidget(
-                                    onPressed: () {
-                                      if (controllerNome!.text.isEmpty ||
-                                          controllerCognome!.text.isEmpty) {
-                                        controllerPassword!.text = '';
-                                        controllerUsername!.text = '';
-                                        Fluttertoast.showToast(
-                                            msg: 'Inserisci nome o cognome!');
-                                      } else {
-                                        var stringGenerated =
-                                            generateRandomString(4);
-                                        controllerUsername!.text =
-                                            '${controllerNome!.text[0].toLowerCase()}.${controllerCognome!.text.replaceAll(' ', '').toLowerCase()}.$stringGenerated@pazienti.mindcare.it';
-                                        controllerPassword!.text =
-                                            generatePassword();
-                                      }
-                                    },
-                                    text: 'Genera credenziali',
-                                    options: FFButtonOptions(
-                                      width: 160,
-                                      height: 40,
-                                      color: FlutterFlowTheme.of(context)
-                                          .primaryColor,
-                                      textStyle: FlutterFlowTheme.of(context)
-                                          .subtitle2
-                                          .override(
-                                            fontFamily: 'IBM Plex Sans',
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.normal,
-                                          ),
-                                      borderSide: const BorderSide(
-                                        color: Colors.transparent,
-                                        width: 1,
-                                      ),
-                                      borderRadius: 8,
-                                    ),
-                                  ),
+                                  ],
                                 ),
                                 Padding(
                                   padding: const EdgeInsetsDirectional.fromSTEB(
                                       0, 15, 0, 5),
                                   child: TextFormField(
-                                    readOnly: true,
                                     validator: (value) {
-                                      if (value == null || value.isEmpty) {
+                                      String val = value!.replaceAll(' ', '');
+                                      if (value.isEmpty || val.isEmpty) {
                                         return 'Inserisci l\'email!';
+                                      }
+                                      RegExp regex = RegExp(
+                                          r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                                      if (!regex.hasMatch(value)) {
+                                        return 'Inserisci un\'email valida!';
                                       }
                                       return null;
                                     },
@@ -484,13 +495,13 @@ class _AggiuntaPazienteWidgetState extends State<AggiuntaPazienteWidget> {
                                           fontSize: 15,
                                           fontWeight: FontWeight.normal,
                                         ),
+                                    keyboardType: TextInputType.emailAddress,
                                   ),
                                 ),
                                 Padding(
                                   padding: const EdgeInsetsDirectional.fromSTEB(
                                       0, 15, 0, 5),
                                   child: TextFormField(
-                                    readOnly: true,
                                     validator: (value) {
                                       RegExp regex = RegExp(
                                           r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%^&*()<>,./-]).{8,20}$');
@@ -563,6 +574,35 @@ class _AggiuntaPazienteWidgetState extends State<AggiuntaPazienteWidget> {
                                           fontSize: 15,
                                           fontWeight: FontWeight.normal,
                                         ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsetsDirectional.fromSTEB(
+                                      0, 15, 0, 10),
+                                  child: FFButtonWidget(
+                                    onPressed: () {
+                                      controllerPassword!.text =
+                                          generatePassword();
+                                    },
+                                    text: 'Genera password',
+                                    options: FFButtonOptions(
+                                      width: 160,
+                                      height: 40,
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryColor,
+                                      textStyle: FlutterFlowTheme.of(context)
+                                          .subtitle2
+                                          .override(
+                                            fontFamily: 'IBM Plex Sans',
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                      borderSide: const BorderSide(
+                                        color: Colors.transparent,
+                                        width: 1,
+                                      ),
+                                      borderRadius: 8,
+                                    ),
                                   ),
                                 ),
                                 Padding(
