34dd5dbd17d701c9bc14abd61ab4839057d16050
Plettrus <s.delsorbo99@gmail.com>
2022-12-14 11:24:22 +0100

    biometria

--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,14 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth_android/local_auth_android.dart';
+import 'package:local_auth_ios/local_auth_ios.dart';
 import 'package:mindcare/caregiver/home_caregiver.dart';
 import 'package:mindcare/paziente/home_paziente.dart';
+
 
 
 import 'auth.dart';
 import 'login.dart';
 import 'package:flutter/material.dart';
 
-class WidgetTree extends StatefulWidget {
+class WidgetTree extends StatefulWidget{
   const WidgetTree({Key? key}) : super(key: key);
 
   @override
@@ -16,7 +21,10 @@ class WidgetTree extends StatefulWidget {
 }
 
 class _WidgetTreeState extends State<WidgetTree> {
+
   Future<String> checkUser() async {
+    print("Controllo l'user");
+    bool isLoggedWithBiometrics = await checkBiometrics();
     QuerySnapshot snapshot = await FirebaseFirestore.instance
         .collection('user')
         .get(); //ottenimento di tutti i documenti nella collezione user
@@ -43,6 +51,43 @@ class _WidgetTreeState extends State<WidgetTree> {
       }
     }
     return '';
+  }
+
+  Future <bool> checkBiometrics() async {
+      final LocalAuthentication _localAuthentication = LocalAuthentication();
+      bool isBiometricSupported = await _localAuthentication.isDeviceSupported();
+      bool canCheckBiometrics = await _localAuthentication.canCheckBiometrics;
+      bool isAuthenticated = false;
+      final List<BiometricType> availableBiometrics = await _localAuthentication.getAvailableBiometrics();
+      if (availableBiometrics.isEmpty)
+        print("la lista di biometria è vuota");
+      else
+        print("la lista è piena" + availableBiometrics.toString());
+      if (availableBiometrics.contains(BiometricType.weak))
+        print("face disponibile [weak]");
+        else
+        print("non trovo face");
+      if (availableBiometrics.contains(BiometricType.strong))
+        print("fingerprint disponibile [strong]");
+      else
+        print("non trovo fingerprint");
+      print("isAuthenticated " + isAuthenticated.toString());
+      print("cancheckBiometrics " + canCheckBiometrics.toString());
+      if (isBiometricSupported && canCheckBiometrics) {
+      try {
+        isAuthenticated = await _localAuthentication.authenticate(
+          localizedReason: 'Per favore procedi con l\'autentificazione prima di utilizzare l\'applicazione',
+          options: const AuthenticationOptions(
+            useErrorDialogs: true,
+            biometricOnly: true,
+            stickyAuth: true));
+        print("ho finito isAuthenticated");
+      } on PlatformException catch (e) {
+        print("stampo l'errore " +  e.toString());
+      }
+    }
+    print("isAuthenticated#2 " + isAuthenticated.toString());
+     return isAuthenticated;
   }
 
   Future<void> deleteUserDB(String type) async {
